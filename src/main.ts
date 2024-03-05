75cfaa656b487b19a041bbb6d1e9f2e95bb53ef4
Ritchie Velmir <ritchievelmir@MacBook-Pro-de-Ritchie.local>
2024-03-05 17:16:12 +0100

    tech: add clean archi infrastructure

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './assets/main.css'
-import './assets/css/tailwind.css';
+import './assets/css/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from './router/router'
 
 const app = createApp(App)
 
