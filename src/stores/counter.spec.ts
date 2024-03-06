6f10978a1eb954785cfd6248fb6756f1ff2cd9c9
Ritchie Velmir <ritchie.elloumi@gmail.com>
2024-03-06 09:54:27 +0100

    tech: add jest for unit tests

---
+++ src/stores/counter.spec.ts
@@ -0,0 +1,9 @@
+describe('Basic Jest Test', () => {
+  test('true should be true', () => {
+    expect(true).toBe(true)
+  })
+
+  it('false should be false', () => {
+    expect(false).toBe(false)
+  })
+})
